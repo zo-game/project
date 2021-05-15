b694a13b3ce49d8b99a86a5e1b0ed00c318bb251
zo-game <68361665+zo-game@users.noreply.github.com>
2021-05-15 23:21:42 +0500

    save 1

---
+++ Assets/Moving.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public Vector2 direction;
+    public int Speed;
+    public GameObject Player;
+    private bool isMoving = true;
+
+    void Update()
+    {
+        transform.Translate(direction * Speed * Time.deltaTime);
+        //var routine = Move();
+        //if (true)
+        //StartCoroutine((IEnumerator)routine);
+        //transform.Translate(direction * Time.deltaTime * Speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //if (collider.tag == "Player")
+        print("AAAAAAAAAAAAAA");
+        Debug.Log("AaAAAAAAA");
+            //Speed = 0;
+        //isMoving = false;
+
+    }
+    //void OnCollisionEnter2D(Collision2D collider)
+    //{
+        
+    //    print("AAAAAAAAAAAAAA");
+    //    //Speed = 0;
+    //    //isMoving = false;
+
+    //}
+
+    //public IEnumerable Move()
+    //{
+    //    //var time = new WaitForSeconds(0.01f);
+    //    //while (isMoving)
+    //    for(var i = 0; i < 10; i++)
+    //    {
+    //        Player.transform.Translate(direction * Time.deltaTime * Speed);
+    //        yield return new WaitForSeconds(0.01f);
+    //    }
+
+    //    //Debug.Log("Movement done");
+    //}
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("GGGGGGGG");
+        print("gggggg");
+    }
+
+    private void OnTriggerEnter2D(Collision2D collision)
+    {
+        Debug.Log("GGGGGGGG");
+        print("gggggg");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
